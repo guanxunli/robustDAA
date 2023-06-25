ee89d7aec16cc4735243b7841dab6049dc30ec1c
Guanxun Li <victor@connect-172-31-32-177.vpn.tamu.edu>
2023-06-25 14:41:58 -0500

    Add a example dataset.

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' A small example out OTU data set with meta information
+#'
+#' The data set was generated using a log-linear model, with error terms generated from a Weibull distribution.
+#'
+#' @format A list consisting of Y as the OTU data set and Z as the metadata set.
+#'
+"dta"
